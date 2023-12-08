588ea18fb73064e71c8c974f59f4231a55881545
CarlosribesDev <carlosribes.dev@gmail.com>
2023-12-09 00:29:51 +0100

    feat: add orm config

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,5 +1,28 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from 'src/config';
 
 @Global()
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { host, user, password, name } = configService.database;
+        return {
+          type: 'mysql',
+          host: host,
+          username: user,
+          password: password,
+          database: name,
+          entities: [],
+          synchronize: true,
+          ssl: {},
+        };
+      },
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
 export class DatabaseModule {}
